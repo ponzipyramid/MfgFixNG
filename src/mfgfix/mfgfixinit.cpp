5e2c20447d31a4691db62caafaccf0ebaaceea17
ponzipyramid <ponzipyramid123@proton.me>
2024-03-09 18:39:01 -0500

    added 1.5 offsets for fixes

--- src/mfgfix/mfgfixinit.cpp
+++ src/mfgfix/mfgfixinit.cpp
@@ -22,12 +22,16 @@ namespace MfgFix
 
 		// Misc
 
-		// allow expression change for dead npcs
-		REL::safe_fill(Offsets::BSFaceGenAnimationData::sub_3DB770.address() + 0x004A, 0x90, 0x0D);
+		// allow expression change for dead npcs - 1.5 seems to use short jumps which necessitates a smaller offset ???
+		REL::safe_fill(Offsets::BSFaceGenAnimationData::sub_3DB770.address() + REL::VariantOffset(0x4B, 0x4A, 0x4B).offset(), 0x90, REL::VariantOffset(0x09, 0x0D, 0x09).offset()); 
+		
 		// fix papyrus function SetExpressionOverride that didn't work with aiMood 0
+		// first offset seems the same
 		REL::safe_write(Offsets::Papyrus::Actor::SetExpressionOverride.address() + 0x001A, static_cast<std::uint16_t>(0x9001));
-		REL::safe_write(Offsets::Papyrus::Actor::SetExpressionOverride.address() + 0x002A, static_cast<std::uint8_t>(0x10));
+		// second is different
+		REL::safe_write(Offsets::Papyrus::Actor::SetExpressionOverride.address() + REL::VariantOffset(0x29, 0x2A, 0x29).offset(), static_cast<std::uint8_t>(0x10));
+		
 		// disable code that sometimes cause ctd because of missing check for null parent node
-		REL::safe_fill(Offsets::BSFaceGenNiNode::sub_3F0C90.address() + 0x0395, 0x90, 0x0F);
+		REL::safe_fill(Offsets::BSFaceGenNiNode::sub_3F0C90.address() + REL::VariantOffset(0x2E3, 0x0395, 0x2E3).offset(), 0x90, 0x0F);
 	}
 }
